8c9a3aad9c550a106a40d7267b0e79a7fa8d5632
RafaelJCamara <rafaelcamarac@gmail.com>
2022-12-03 22:09:17 +0000

    Add Identity support.

---
+++ src/Play.Common/Play.Common/Identity/ConfigureJwtBearerOptions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Play.Common.Settings;
+
+namespace Play.Common.Identity
+{
+    public class ConfigureJwtBearerOptions : IConfigureNamedOptions<JwtBearerOptions>
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfigureJwtBearerOptions(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Configure(string name, JwtBearerOptions options)
+        {
+            if(name == JwtBearerDefaults.AuthenticationScheme)
+            {
+                var serviceSettings = configuration
+                                        .GetSection(nameof(ServiceSettings))
+                                        .Get<ServiceSettings>();
+                options.Authority = serviceSettings.Authority;
+                options.Audience = serviceSettings.ServiceName;
+                /*
+                    This definition below is so that we don't have any problems integrating with other identity providers
+                 */
+                options.MapInboundClaims = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
+            }
+        }
+
+        public void Configure(JwtBearerOptions options)
+        {
+            Configure(Options.DefaultName, options);
+        }
+    }
+}
--- src/Play.Common/Play.Common/Settings/ServiceSettings.cs
+++ src/Play.Common/Play.Common/Settings/ServiceSettings.cs
@@ -4,5 +4,6 @@
     {
         //We use init here because we don't want to change it after it is configured
         public string ServiceName { get; init; }
+        public string Authority { get; init; }
     }
 }
