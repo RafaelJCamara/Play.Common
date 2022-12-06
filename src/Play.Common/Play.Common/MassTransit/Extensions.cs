682ed47380c2e5d994592cc965b7c645ef0d85d3
RafaelJCamara <rafaelcamarac@gmail.com>
2022-12-06 12:23:41 +0000

    Preparig the Saga participants

--- src/Play.Common/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/Play.Common/MassTransit/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using GreenPipes;
+using GreenPipes.Configurators;
 using MassTransit;
 using MassTransit.Definition;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Play.Common.Settings;
@@ -15,28 +17,15 @@ namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        public static IServiceCollection AddMassTransitWithRabbitMq(
+            this IServiceCollection services,
+            Action<IRetryConfigurator> configureRetries = null)
         {
             services
                 .AddMassTransit(configure =>
                 {
                     configure.AddConsumers(Assembly.GetEntryAssembly());
-
-                    configure.UsingRabbitMq((context, configurator) =>
-                    {
-                        var configuration = context.GetService<IConfiguration>();
-                        var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                        var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-                        configurator.Host(rabbitMQSettings.Host);
-                        //helps us in the creation of rabbitMQ queues
-                        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-                        //this means that we are retrying if, by any chance, the consumer of the message could not consume it successfully
-                        configurator.UseMessageRetry(retryConfigurator =>
-                        {
-                            //it will try to consume the message 3 times, with a time difference between them of 5 seconds
-                            retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
-                        });
-                    });
+                    configure.UsingPlayEconomyRabbitMq(configureRetries);
                 });
 
             services
@@ -44,5 +33,34 @@ namespace Play.Common.MassTransit
 
             return services;
         }
+
+        public static void UsingPlayEconomyRabbitMq(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                configurator.Host(rabbitMQSettings.Host);
+                //helps us in the creation of rabbitMQ queues
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                ////this means that we are retrying if, by any chance, the consumer of the message could not consume it successfully
+                //configurator.UseMessageRetry(retryConfigurator =>
+                //{
+                //    //it will try to consume the message 3 times, with a time difference between them of 5 seconds
+                //    retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                //});
+
+                if(configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
+        }
+
     }
 }
