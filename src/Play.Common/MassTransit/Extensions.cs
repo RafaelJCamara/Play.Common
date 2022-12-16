2de896463324330aec0c23ff902f112d8db05576
Rafael Camara <rafaelcamarac@gmail.com>
2022-12-16 17:54:37 +0000

    Add support for service bus.

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -17,6 +17,29 @@ namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
+
+        private const string RabbitMq = "RABBITMQ";
+        private const string ServiceBus = "SERVICEBUS";
+
+        public static IServiceCollection AddMassTransitWithessageBroker(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            switch (serviceSettings.MessageBroker?.ToUpperInvariant())
+            {
+                case ServiceBus:
+                    services.AddMassTransitWithServiceBus(configureRetries);
+                    break;
+                case RabbitMq:
+                default:
+                    services.AddMassTransitWithRabbitMq(configureRetries);
+                    break;
+            }
+            return services;
+        }
+
         public static IServiceCollection AddMassTransitWithRabbitMq(
             this IServiceCollection services,
             Action<IRetryConfigurator> configureRetries = null)
@@ -34,9 +57,62 @@ namespace Play.Common.MassTransit
             return services;
         }
 
-        /*
-            This method here is just the specification/configuration of the RabbitMQ
-         */
+        public static IServiceCollection AddMassTransitWithServiceBus(
+            this IServiceCollection services,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            services
+                .AddMassTransit(configure =>
+                {
+                    configure.AddConsumers(Assembly.GetEntryAssembly());
+                    configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+                });
+
+            services
+                .AddMassTransitHostedService();
+
+            return services;
+        }
+
+        public static void UsingPlayEconomyMessageBroker(
+            this IServiceCollectionBusConfigurator configure,
+            IConfiguration configuration,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            switch (serviceSettings.MessageBroker?.ToUpperInvariant())
+            {
+                case ServiceBus:
+                    configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+                    break;
+                case RabbitMq:
+                default:
+                    configure.UsingPlayEconomyRabbitMq(configureRetries);
+                    break;
+            }
+        }
+
+        public static void UsingPlayEconomyAzureServiceBus(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingAzureServiceBus((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceBusSettings = configuration.GetSection(nameof(ServiceBusSettings)).Get<ServiceBusSettings>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                configurator.Host(serviceBusSettings.ConnectionString);
+
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
+        }
+
         public static void UsingPlayEconomyRabbitMq(
             this IServiceCollectionBusConfigurator configure,
             Action<IRetryConfigurator> configureRetries = null)
@@ -49,14 +125,14 @@ namespace Play.Common.MassTransit
                 configurator.Host(rabbitMQSettings.Host);
                 //helps us in the creation of rabbitMQ queues
                 configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-                ////this means that we are retrying if, by any chance, the consumer of the message could not consume it successfully
+                //this means that we are retrying if, by any chance, the consumer of the message could not consume it successfully
                 //configurator.UseMessageRetry(retryConfigurator =>
                 //{
-                //    //it will try to consume the message 3 times, with a time difference between them of 5 seconds
+                // it will try to consume the message 3 times, with a time difference between them of 5 seconds
                 //    retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
                 //});
 
-                if(configureRetries == null)
+                if (configureRetries == null)
                 {
                     configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
                 }
--- src/Play.Common/Settings/ServiceSettings.cs
+++ src/Play.Common/Settings/ServiceSettings.cs
@@ -5,5 +5,6 @@
         //We use init here because we don't want to change it after it is configured
         public string ServiceName { get; init; }
         public string Authority { get; init; }
+        public string MessageBroker { get; init; }
     }
 }
